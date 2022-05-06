254ac1ea022514b9f813b9d7c73b045e701a9292
Richard Venite <richard.miguez@gmail.com>
2022-05-06 04:30:22 -0300

    fix: #7 fix comparation field

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -25,24 +25,24 @@ class UserController extends Controller
      */
     public function transaction(Request $request) {
         $validator = Validator::make($request->all(),[
-            'payer' => 'required|string|max:11|min:11|exists:users,identity|different:toIdentity',
+            'payer' => 'required|string|max:11|min:11|exists:users,identity|different:payee',
             'payee' => 'required|string|max:14|min:11|exists:users,identity',
             'value' => 'required|numeric|gt:0|regex:/^\d{1,13}(\.\d{1,2})?$/'
         ]);
-
-        $payer = $request->payer;
-        $payee = $request->payee;
-        $value = $request->value;
 
         if ($validator->fails()){
             return response()->json(['error' => $validator->errors()], 400);       
         }
 
-        if (!$this->userService->hasCashWallet($payer, $value)) {
+        $payer = $request->payer;
+        $payee = $request->payee;
+        $value = $request->value;
+
+        if (!$this->getService()->hasCashWallet($payer, $value)) {
             return response()->json(['error' => ["payer" => 'Payer doesn\'t have enough value in wallet']], 400);
         }
 
-        $result = $this->userService->transfer($payer, $payee, $value);
+        $result = $this->getService()->transfer($payer, $payee, $value);
 
         if (!$result) {
             return response()->json(['error' => 'Unexpected error please try again'], 500);
@@ -55,5 +55,10 @@ class UserController extends Controller
         ];
 
         return response()->json($response);
+    }
+
+    public function getService()
+    {
+        return $this->userService;
     }
 }
